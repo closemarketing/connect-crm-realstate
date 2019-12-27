84ea43221fb691184ab08c32e865cb97647c6466
David Perez <david@closemarketing.es>
2019-12-27 13:24:01 +0100

    conexión api realizada

--- includes/class-iip-admin.php
+++ includes/class-iip-admin.php
@@ -169,7 +169,13 @@ class IIP_Admin {
 		</div>
 		<?php
 		if ( $show_merge_vars ) {
-			$this->get_properties();
+
+			$args_query = array( 'paginacion', 1, 1, 'ascensor=1', 'precioinmo, precioalq' );
+			$property_base = $this->get_properties( $args_query );
+
+			echo '<pre>';
+			print_r($property_base['paginacion'][1]);
+			echo '</pre>';
 			?>
 			<div class="wrap">
 				<h1><?php esc_html_e( 'Merge Variables with custom values', 'import-inmovilla-properties'); ?></h1>
@@ -196,31 +202,37 @@ class IIP_Admin {
 	 *
 	 * @return array
 	 */
-	private function get_properties() {
+	private function get_properties( $query_array ) {
 		$agency_number = get_option( 'iip_agency_number' );
 		$agency_pass   = get_option( 'iip_agency_pass' );
+		$language      = get_option( 'iip_language' );
 
+
+		if ( '2' === $agency_number ) {
+			$ia = '84.120.176.252';
+			$ib = '42.5.120.1';
+		} else {
+			$ia = isset( $_SERVER['REMOTE_ADDR'] ) ? esc_url_raw( wp_unslash( $_SERVER['REMOTE_ADDR'] ) ) : '';
+			$ib = isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ? esc_url_raw( wp_unslash( $_SERVER['HTTP_X_FORWARDED_FOR'] ) ) : '';
+		}
+		$query_text = $agency_number . ';' . $agency_pass . ';' . $language . ';lostipos';
+		foreach ( $query_array as $query_item ) {
+			$query_text = $query_text . ';' . $query_item;
+		}
+		$query_text   = rawurlencode( $query_text );
+		$query_string = 'param=' . $query_text . '&json=1';
+
+		// API connection.
 		$args = array(
 			'headers' => array(
 				'Accept'     => 'text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
 				'Connection' => 'keep-alive'
 			),
-			'body'    => array(
-				'numagencia' => $agency_number,
-				'password'   => $agency_pass,
-				'idioma'     => 1,
-				'ia'         => '84.120.210.5',
-				'ib'         => '42.5.120.1',
-			),
 		);
-		$response = wp_remote_get( 'http://apiweb.inmovilla.com/apiweb/apiweb_demo.php', $args );
-		echo '<pre>';
-		print_r($response);
-		echo '</pre>';
+		$response = wp_remote_get( 'http://apiweb.inmovilla.com/apiweb/apiweb_demo.php?' . $query_string, $args );
 		if ( 200 === $response['response']['code'] ) {
 			$body = wp_remote_retrieve_body( $response );
-
-			return $body;
+			return json_decode( $body, true );
 		} else {
 			return false;
 		}
