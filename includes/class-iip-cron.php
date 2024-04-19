7f2913873c54f0fef3568cfbb5891da80152617f
davidperezgar <david@closemarketing.es>
2024-04-19 17:35:24 +0200

    prevents sync time

--- includes/class-iip-cron.php
+++ includes/class-iip-cron.php
@@ -52,9 +52,14 @@ class Cron {
 	 */
 	public function cron_sync_properties() {
 		$time_start = microtime( true );
-		$date_since = strtotime( 'now - ' . CCRMRE_SYNC_PERIOD . ' seconds' );
-		$date_since = gmdate( 'Y/m/d H:i:s', $date_since );
-		$result_api = API::get_properties( 0, $date_since );
+
+		// Gets data from last sync.
+		$last_sync = get_option( 'ccrmre_cron_sync_last_time' );
+		if ( empty( $last_sync ) ) {
+			$date_since = strtotime( 'now - ' . CCRMRE_SYNC_PERIOD . ' seconds' );
+			$last_sync  = gmdate( 'Y/m/d H:i:s', $date_since );
+		}
+		$result_api = API::get_properties( 0, $last_sync );
 
 		if ( 'error' === $result_api['status'] || empty( $result_api['data'] ) ) {
 			return;
@@ -64,6 +69,8 @@ class Cron {
 			$result_log[] = SYNC::sync_property( $property );
 		}
 		$this->save_log( $time_start, $result_log );
+
+		update_option( 'ccrmre_cron_sync_last_time', gmdate( 'Y/m/d H:i:s' ) );
 		return array(
 			'synced' => count( $result_api['data'] ),
 		);
