dd070b4aa89b18a848c70cf5671d16bac8ad5ab0
davidperezgar <david@closemarketing.es>
2026-01-24 17:03:45 +0100

    version

--- connect-crm-realstate.php
+++ connect-crm-realstate.php
@@ -5,7 +5,7 @@
  * Description: Connect Properties from Inmovilla/Anaconda to a Custom Post Type.
  * Author: closemarketing
  * Author URI: https://close.technology/
- * Version: 1.0.0-beta.12
+ * Version: 1.0.0-beta.13
  *
  * @package WordPress
  * Text Domain: connect-crm-realstate
@@ -14,7 +14,7 @@
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  */
 
-define( 'CCRMRE_VERSION', '1.0.0-beta.12' );
+define( 'CCRMRE_VERSION', '1.0.0-beta.13' );
 define( 'CCRMRE_PLUGIN', __FILE__ );
 define( 'CCRMRE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'CCRMRE_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
